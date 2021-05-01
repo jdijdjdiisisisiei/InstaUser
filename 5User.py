7c5b0a3f614e0b0f7ce12f8b0b15cb4b4dbcc08c
Abdrrahman Ameur <80923235+whisper-666@users.noreply.github.com>
2021-05-01 19:09:25 +0100

    Add files via upload

---
+++ 5User.py
@@ -0,0 +1,139 @@
+import requests
+import uuid
+import sys
+import os
+from random import *
+import colorama 
+from colorama import *
+import time 
+import string 
+from user_agent import generate_user_agent
+import random
+os.system('clear')
+ruksI='IdMN1w9spls'
+rg = 'D'
+#@DIBIBl
+h = '_' 
+ruks_ = '\033[1;36m'
+rf = 'l'
+ry = 'B'
+ruks__ = '\033[1;36m'
+ruksj='VCUHJ2xP1'
+tuks1 = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+jruks = '\033[1;33m'
+_ruks  = '\033[1;31m'
+BGreen='\033[1;32m'
+BRed ='\033[1;31m'
+ruksl='AAGgq9CpeCNh'
+#@DIBIBl
+token = '1468816115:AAEIp-9MZhDzrAiUtU-52IUSGQB-jnqFfN0'
+logo=choice(["""{w}
+   ^     . .     ^  
+   \ \    V    / /  
+   \   \(+ +)/   / {r}({y}Programmer{r}){rr}━━━━━━━━━━{r}({y}whisper{r}){w} 
+   \   (  v  )   / {r}({y}Facebook{r}){rr}━━━━━━━━━{r}({y}Abdrrahman Ameur{w})
+    \____m_m____/  {r}({y}Instagram{r}){rr}━━━━━━━{r}({y}@Abdrrahman._.Ameur{r}){w}
+   <[___________]> {r}({y}WhatsApp{r}){rr}━━━━━━━{r}({y}+213673336272{r}){w}
+  
+""".format(r=ruks__,y=jruks,rr=_ruks,w=ruks_,D=rg,l=rf,B=ry),#@DIBIBl
+"""                                              {y}                 
+      
+@@@  @@@  @@@   @@@@@@   @@@@@@@   @@@@@@   
+@@@  @@@@ @@@  @@@@@@@   @@@@@@@  @@@@@@@@  
+@@!  @@!@!@@@  !@@         @@!    @@!  @@@  
+!@!  !@!!@!@!  !@!         !@!    !@!  @!@  
+!!@  @!@ !!@!  !!@@!!      @!!    @!@!@!@!  
+!!!  !@!  !!!   !!@!!!     !!!    !!!@!!!!  
+!!:  !!:  !!!       !:!    !!:    !!:  !!!  
+:!:  :!:  !:!      !:!     :!:    :!:  !:!  
+ ::   ::   ::  :::: ::      ::    ::   :::  
+:    ::    :   :: : :       :      :   : :                                 
+{rr}𖢌{r} Programmer   {rr}:{y}whisper
+{rr}𖢌{r} Facebook     {rr}:{y}Abdrrahman Ameur
+{rr}𖢌{r} Instagram    {rr}:{y}@Abdrrahman._.Ameur
+{rr}𖢌{r} WhatsApp     {rr}:{y}+213673336272
+""".format(r=ruks__,y=jruks,rr=_ruks,w=ruks_,D=rg,l=rf,B=ry),
+"""                                                 {y} 
+      _____           _                    
+     |_   _|         | |                   
+       | |  _ __  ___| |_ __ _             
+       | | | '_ \/ __| __/ _` |            
+      _| |_| | | \__ \ || (_| |            
+     |_____|_| |_|___/\__\__,_|          
+            
+{rr}\\_{r}Programmer{rr}>{y}whisper
+{rr}\\_{r}Facebook{rr}>{y}Abdrrahman Ameur
+{rr}\\_{r}Instagram{rr}>{y}@Abdrrahman._.Ameur
+{rr}\\_{r}WhatsApp{rr}:{y}+213673336272
+""".format(r=ruks__,y=jruks,rr=_ruks,w=ruks_,D=rg,l=rf,B=ry),
+])
+print(logo)
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+print(jruks+'['+_ruks+'1'+jruks+']'+ruks__+' running Checker')
+print(jruks+'['+_ruks+'2'+jruks+']'+ruks__+' Developer Instagram')
+print(jruks+'['+_ruks+'3'+jruks+']'+ruks__+' Telegram BOT ')
+print('')
+ruks = input(jruks+'['+_ruks+'+'+jruks+']'+ruks__+' Choose ! -> ; '+BGreen)
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+ru = ('{"account_created": false, "errors": {"email": [{"message": "Too many accounts are using a@gmail.com.", "code": "email_sharing_limit"}], "__all__": [{"message": "Create a password at least 6 characters long.", "code": "too_short_password"}]}, "dryrun_passed": false, "username_suggestions": [], "status": "ok", "error_type": "form_validation_error"}')
+ruksz=' 𝐓𝐞𝐥𝐞 :@RUKS3 ☆ @DIBIBl'
+def Code_ruks():    
+    if ruks =='1':
+        uid = input("Telegram ID » ")
+        print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        Number = 0
+        while True:
+            user = str("".join(random.choice(tuks1)for i in range(5)))
+            us = str("".join(random.choice(h+user)for i in range(5)))            
+            url = 'https://www.instagram.com/accounts/web_create_ajax/attempt/'
+            heade_ruks={
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'ar,en-US;q=0.9,en;q=0.8,ar-SA;q=0.7',
+                'content-length': '61',
+                'content-type': 'application/x-www-form-urlencoded',
+                'cookie': 'ig_cb=2; ig_did=BB52B198-B05A-424E-BA07-B15F3D4C3893; mid=YAlcaQALAAHzmX6nvD8dWMRVYFCO; shbid=15012; rur=PRN; shbts=1612894029.7666144; csrftoken=CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'origin': 'https://www.instagram.com',
+                'referer': 'https://www.instagram.com/accounts/emailsignup/',
+                'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+                'x-csrftoken': 'CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'x-ig-app-id': '936619743392459',
+                'x-ig-www-claim': 'hmac.AR0Plwj5om112fwzrrYnMNjMLPnyWfFFq1tG7MCcMv5_vN9M',
+                'x-instagram-ajax': '72bda6b1d047',
+                'x-requested-with': 'XMLHttpRequest'
+
+            } 
+#The data shown above are identical Do not touch anything developer Ruks          
+            data_ruks={
+                'email' : 'a@gmail.com',
+                'username': f'{us}',
+                'first_name': 'AA',
+                'opt_into_one_tap': 'false'
+            }
+#ruks DIBIBl
+            da = requests.post(url,headers=heade_ruks,data=data_ruks).text
+            if ru in  da:
+                Number+=1
+                tlg =(f'https://api.telegram.org/bot1727781188:AAEO-WV8HUbhciNGiZYdQeuWbpakNqBLPTg/sendMessage?chat_id={uid}&text=👑Avaliable User <{us}>\n━━━━━━━━━━━━━\n• Tele : @itzthedevil ؟ ، 🔥')
+                req = requests.post(tlg)	
+                print(jruks+'['+BGreen+f'{Number}'+jruks+'] Available'+BGreen+f' [{us}]')
+                with open('5User.txt', 'a') as x:
+                	x.write(us+ '\n')         
+#@DIBIBl                
+            else:
+                Number+=1
+                print(jruks+'['+BRed+f'{Number}'+jruks+'] Unavailable'+BRed+f'-[{us}]')
+
+if ruks =='2':
+	os.system('xdg-open https://Instagram.com/abdrrahman._.ameur')
+if ruks =='3':
+	os.system('xdg-open https://t.me/uinstauserbot')
+#@DIBIBl	
+else:
+	print('')
+Code_ruks()           
